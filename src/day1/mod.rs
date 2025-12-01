800afcde41e6dbfadea89396897781c1138e03c5
Harrison Shelnutt <harrison.shelnutt@agilebits.com>
2025-12-01 14:30:09 -0700

    scaffolding

---
+++ src/day1/mod.rs
@@ -0,0 +1,13 @@
+const INPUT: &str = include_str!("input.txt");
+
+fn solve_part_one() -> usize {
+    0
+}
+
+fn solve_part_two() -> usize {
+    0
+}
+
+pub fn solve() {
+    println!("part 1: {}, part 2: {}", solve_part_one(), solve_part_two())
+}
---
+++ src/main.rs
@@ -0,0 +1,33 @@
+use clap::Parser;
+
+mod day1;
+
+#[derive(Parser)]
+struct Args {
+    /// Which day's problem to solve
+    #[arg(long, value_parser = parse_day)]
+    day: u8,
+}
+
+fn parse_day(day: &str) -> Result<u8, String> {
+    let day: u8 = day
+        .parse()
+        .map_err(|_| format!("Expected numeric value in range [1, 12], got '{}'", day))?;
+
+    if !(1..=12).contains(&day) {
+        return Err(format!(
+            "Expected numeric value in range [1, 12], got '{}'",
+            day
+        ));
+    }
+
+    Ok(day)
+}
+
+fn main() {
+    let args = Args::parse();
+    match args.day {
+        1 => day1::solve(),
+        _ => panic!("day {} is not yet implemented", args.day),
+    }
+}
